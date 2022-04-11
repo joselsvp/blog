90383e6292c75776dad2673e8c0881d77a968323
Jose Villanueva <lv428694@gmail.com>
2022-04-10 21:21:20 -0500

    add models relations between tables

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    //relación uno a muchos
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //Relación uno a muchos inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relación muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relación uno a uno polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
